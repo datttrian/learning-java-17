0daadc4729a4c762b075460c8cc7f6ac540de794
datttrian <datttrian@gmail.com>
2023-10-01 23:45:59 +0000

    Decision-making with if in Java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,44 +2,16 @@ import java.util.Scanner;
 
 public class HelloWorld {
   public static void main(String[] args) {
-    // System.out.println("Hello World! It's me, Kathryn");
-
-    // int studentAge = 15;
-    // double studentGPA = 3.45;
-    // char studentFirstInitial = 'K';
-    // char studentLastInitial = 'H';
-    // boolean hasPerfectAttendance = true;
-    // String studentFirstName = "Kayla";
-    // String studentLastName = "Harley";
-    // char studentFirstInitial = studentFirstName.charAt(0);
-    // char studentLastInitial = studentLastName.charAt(0);
-    double studentGPA = 3.45;
-    String studentFirstName = "Kayla";
-    String studentLastName = "Harley";
-
-    // System.out.println(studentAge);
-    // System.out.println(studentGPA);
-    // System.out.println(studentFirstInitial);
-    // System.out.println(studentLastInitial);
-    // System.out.println(hasPerfectAttendance);
-    // System.out.println(studentName);
-    // System.out.println(studentFirstName);
-    // System.out.println(studentLastName);
-    // System.out.println(studentFirstInitial);
-    // System.out.println(studentLastInitial);
-    System.out.println(studentFirstName +
-        " " +
-        studentLastName +
-        " has a GPA of " +
-        studentGPA +
-        ".");
-
-    System.out.println("What do you want to update it to?");
-
+    System.out.println("Pick a number between 1 and 10");
     Scanner input = new Scanner(System.in);
-    studentGPA = input.nextDouble();
-    System.out.println(studentFirstName + " " +
-        studentLastName + " now has a GPA of " + studentGPA);
+    int inputtedNum = input.nextInt();
+
+    if (inputtedNum < 5) {
+      System.out.println("Enjoy the good luck a friend brings you");
+    } else {
+      System.out.println("Your shoe selection will make you happy today");
+    }
+
   }
 
 }
