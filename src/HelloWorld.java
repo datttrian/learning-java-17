eb780f5db7d879732bc3f80a8c700eba5043fff8
datttrian <datttrian@gmail.com>
2023-10-01 22:51:53 +0000

    Strings in Java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,17 +2,21 @@ public class HelloWorld {
   public static void main(String[] args) {
     // System.out.println("Hello World! It's me, Kathryn");
 
-    int studentAge = 15;
-    double studentGPA = 3.45;
-    char studentFirstInitial = 'K';
-    char studentLastInitial = 'H';
-    boolean hasPerfectAttendance = true;
+    // int studentAge = 15;
+    // double studentGPA = 3.45;
+    // char studentFirstInitial = 'K';
+    // char studentLastInitial = 'H';
+    // boolean hasPerfectAttendance = true;
+    String studentFirstName = "Kayla";
+    String studentLastName = "Harley";
 
-    System.out.println(studentAge);
-    System.out.println(studentGPA);
-    System.out.println(studentFirstInitial);
-    System.out.println(studentLastInitial);
-    System.out.println(hasPerfectAttendance);
-
+    // System.out.println(studentAge);
+    // System.out.println(studentGPA);
+    // System.out.println(studentFirstInitial);
+    // System.out.println(studentLastInitial);
+    // System.out.println(hasPerfectAttendance);
+    // System.out.println(studentName);
+    System.out.println(studentFirstName);
+    System.out.println(studentLastName);
   }
 }
