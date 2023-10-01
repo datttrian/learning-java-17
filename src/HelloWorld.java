df9f6fa28da5b50a520163e52b03bf4e2c441f01
datttrian <datttrian@gmail.com>
2023-10-01 23:00:44 +0000

    Concatenating strings

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -7,10 +7,13 @@ public class HelloWorld {
     // char studentFirstInitial = 'K';
     // char studentLastInitial = 'H';
     // boolean hasPerfectAttendance = true;
+    // String studentFirstName = "Kayla";
+    // String studentLastName = "Harley";
+    // char studentFirstInitial = studentFirstName.charAt(0);
+    // char studentLastInitial = studentLastName.charAt(0);
+    double studentGPA = 3.45;
     String studentFirstName = "Kayla";
     String studentLastName = "Harley";
-    char studentFirstInitial = studentFirstName.charAt(0);
-    char studentLastInitial = studentLastName.charAt(0);
 
     // System.out.println(studentAge);
     // System.out.println(studentGPA);
@@ -18,9 +21,15 @@ public class HelloWorld {
     // System.out.println(studentLastInitial);
     // System.out.println(hasPerfectAttendance);
     // System.out.println(studentName);
-    System.out.println(studentFirstName);
-    System.out.println(studentLastName);
-    System.out.println(studentFirstInitial);
-    System.out.println(studentLastInitial);
+    // System.out.println(studentFirstName);
+    // System.out.println(studentLastName);
+    // System.out.println(studentFirstInitial);
+    // System.out.println(studentLastInitial);
+    System.out.println(studentFirstName +
+        " " +
+        studentLastName +
+        " has a GPA of " +
+        studentGPA +
+        ".");
   }
 }
