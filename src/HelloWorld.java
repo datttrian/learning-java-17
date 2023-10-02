aedfe9b2c1ed50218e5eac60d7cb525c2fb8da99
datttrian <datttrian@gmail.com>
2023-10-02 00:30:43 +0000

    Debugging with print statements

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,21 +2,43 @@ import java.util.Scanner;
 
 public class HelloWorld {
 
-  public static void main(String[] args) {
+  public static void main(String args[]) {
+    String question = "What is the largest planet in our solar system";
+    String choiceOne = "earth";
+    String choiceTwo = "jupiter";
+    String choiceThree = "saturn";
+
+    String correctAnswer = choiceTwo;
+
+    // Write a print statement asking the question
+    System.out.println(question);
+
+    // Write a print statement giving the answer choices
+    System.out.println("Choose one of the following: " +
+        choiceOne + ", " + choiceTwo + ", or " +
+        choiceThree + ".");
+
+    // Have the user input an answer
+    // Retrieve the user's input
     Scanner scanner = new Scanner(System.in);
-    boolean isOnRepeat = true;
+    String input = scanner.next();
 
-    while (isOnRepeat) {
-      System.out.println("Playing current song");
-      System.out.println("Would you like to take this song " +
-          "off of repeat? If so, answer yes");
-      String input = scanner.next();
+    System.out.println("User's answer " + input);
+    System.out.println("Correct answer " + correctAnswer);
 
-      if ("yes".equals(input)) {
-        isOnRepeat = false;
-      }
+    // If the user's input matches the correctAnswer...
+    if (correctAnswer.equals(input.toLowerCase())) {
+      // then the user is correct and we want to print
+      // out a congrats message to the user.
+      System.out.println("Congrats! That’s the correct answer.");
+    } else {
+      // If the user's input does not match the correctAnswer…
+      // then the user is incorrect and we want to print out a
+      // message saying that the user is incorrect as well as
+      // what the correct choice was.
+      System.out.println("You are incorrect. The correct answer is "
+          + correctAnswer);
     }
 
-    System.out.println("Playing next song");
   }
 }
