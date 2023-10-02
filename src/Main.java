c8de32524854118426e8a24ccd8733732d168c97
datttrian <datttrian@gmail.com>
2023-10-02 15:53:19 +0000

    Using instance and class methods in Java

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,11 @@ public class Main {
                 8, 17);
         Triangle triangleB = new Triangle(3, 2.598,
                 3, 3, 3);
+
         double triangleAArea = triangleA.findArea();
-        // Triangle.findArea() --> Math.pow(2, 3)?
+        System.out.println(triangleAArea);
+
+        double triangleBArea = triangleB.findArea();
+        System.out.println(triangleBArea);
     }
 }
