5a6715a869e7bca50e2eccd7f7e54322e3ffa89e
datttrian <datttrian@gmail.com>
2023-10-02 00:46:56 +0000

    Return types in Java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,15 +1,20 @@
 public class HelloWorld {
 
-  public static void calculateTotalMealPrice(double listedPrice,
+  public static double calculateTotalMealPrice(double listedPrice,
       double tipRate,
       double taxRate) {
     double tip = tipRate * listedPrice;
     double tax = taxRate * listedPrice;
     double result = listedPrice + tip + tax;
-    System.out.println("Your total meal price is " + result);
+    return result;
   }
 
   public static void main(String[] args) {
-    calculateTotalMealPrice(25, .18, .08);
+    double groupTotalMealCost = calculateTotalMealPrice(100,
+        .2, .08);
+    System.out.println(groupTotalMealCost);
+
+    double individualTotalMealCost = groupTotalMealCost / 5;
+    System.out.println(individualTotalMealCost);
   }
 }
